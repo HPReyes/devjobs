3f139407aaf53e4261c3cdd29d4bd05ca67d9c7c
HPReyes <hpreyesdev@gmail.com>
2024-03-14 17:28:09 +0100

    alertas

---
+++ resources/views/livewire/editar-vacante.blade.php
@@ -0,0 +1,113 @@
+<form action="" class="md:w-1/2 space-y-5" wire:submit.prevent='editarVacante'>
+    <div>
+        <x-input-label for="titulo" :value="__('Título Vacante')" />
+
+        <x-text-input 
+            id="titulo" 
+            class="block mt-1 w-full" 
+            type="text" 
+            wire:model="titulo" 
+            :value="old('titulo')" 
+            placeholder="Título vacante" />
+
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="salario" :value="__('Salario Mensual')" />
+
+        <select wire:model="salario" id="salario" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 w-full" >
+            <option value="">-- Seleccione --</option>
+            @foreach ($salarios as $salario)
+                <option value="{{$salario->id}}">{{$salario->salario}}</option>
+
+            @endforeach
+        </select>
+
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="categoria" :value="__('Categoría')" />
+
+        <select wire:model="categoria" id="categoria" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 w-full" >
+            <option value="">-- Seleccione --</option>
+            @foreach ($categorias as $categoria)
+                <option value="{{$categoria->id}}">{{$categoria->categoria}}</option>
+
+            @endforeach
+        </select>
+
+        <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+
+        <x-text-input 
+            id="empresa" 
+            class="block mt-1 w-full" 
+            type="text" 
+            wire:model="empresa" 
+            :value="old('empresa')" 
+            placeholder="Título empresa" />
+
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Último día para postularse')" />
+
+        <x-text-input 
+            id="ultimo_dia" 
+            class="block mt-1 w-full" 
+            type="date" 
+            wire:model="ultimo_dia" 
+            :value="old('ultimo_dia')" 
+            placeholder="Título empresa" />
+
+        <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="descripcion" :value="__('Descripción puesto')" />
+
+        <textarea 
+        placeholder="Descripcion general del puesto"
+
+        wire:model="descripcion" 
+        class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring-indigo-200 focus:ring-opacity-50 w-full h-72"></textarea>
+
+        <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
+    </div>
+
+
+<div>
+
+    <x-input-label for="imagen" :value="__('Imagen')" />
+
+    <div class="my-5">
+        <x-text-input 
+        id="imagen" 
+        class="block mt-1 w-full" 
+        type="file" 
+        wire:model="imagen_nueva" 
+        accept="image/*"
+        />
+
+        <x-input-label for="imagen" :value="__('Imagen actual')" />
+        <img src="{{asset('storage/vacantes/'.$imagen)}}" alt="{{'Imagen vacante '. $titulo}}">
+
+
+        <x-input-error :messages="$errors->get('imagen_nueva')" class="mt-2" />
+            <div class="my-5">
+                @if ($imagen_nueva)
+                    Imagen nueva:
+                    <img class="my-5 w-80 mx-auto" src="{{$imagen_nueva->temporaryUrl()}}" alt="imagen-nueva">
+                @endif
+            </div>
+
+    </div>
+
+
+    <x-primary-button class=" w-full justify-center ">
+        {{ __('Guardar cambios') }}
+    </x-primary-button>
+    
+</form>
+</div>
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,44 +1,74 @@
-
 <div>
 
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        
-    @forelse ($vacantes as $vacante)
-    <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">      
-        <div class="space-y-3">
-            <a href="" class="text-xl font-bold">
-                {{$vacante->titulo}}
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
 
-            </a>
-            <p class="text-sm text-gray-600 font-bold">{{$vacante->empresa}}</p>
-            <p class="text-sm text-gray-500">Último día: {{$vacante->ultimo_dia->format('d/m/Y')}}</p>
-        </div>  
-        <div class="flex flex-col md:flex-row gap-3 items-streech mt-5 md:mt-0">
-            <a href=""
-            class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-            Candidatos
-            </a>
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="space-y-3">
+                    <a href="" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
 
-            <a href=""
-            class="bg-blue-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-            Editar
-            </a>
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold">{{ $vacante->empresa }}</p>
+                    <p class="text-sm text-gray-500">Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+                </div>
+                <div class="flex flex-col md:flex-row gap-3 items-streech mt-5 md:mt-0">
+                    <a href=""
+                        class="bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Candidatos
+                    </a>
 
-            <a href=""
-            class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-            Eliminar
-            </a>
-    </div>
-    </div>
+                    <a href="{{ route('vacantes.edit', $vacante->id) }}"
+                        class="bg-blue-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+                        Editar
+                    </a>
+
+                    <button 
+                    wire:click="$dispatch('mostrarAlerta',{ vacante: {{ $vacante->id }} })"
+                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center"
+                >Eliminar</button>
+                </div>
+            </div>
 
         @empty
-        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
+            <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar</p>
         @endforelse
     </div>
-    
-<div class="mt-10">
-            {{$vacantes->links()}}
-        </div>
+
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
 
 
 </div>
+@push('scripts')
+ 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+ 
+    <script>
+        document.addEventListener('livewire:init', () => {
+            Livewire.on('mostrarAlerta', vacante => {
+                Swal.fire({
+                    title: '¿Seguro?',
+                    text: "¡No podrás revertirlo!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: '¡Sí, bórralo!',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Livewire.dispatch('eliminarVacante',[vacante]);
+ 
+                        Swal.fire(
+                            '¡Borrado!',
+                            'Tu vacante ha sido eliminado.',
+                            'success'
+                        )
+                    }
+                });
+       });
+    });
+    </script>
+@endpush
